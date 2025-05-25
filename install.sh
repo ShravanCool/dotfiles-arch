776e7d567e1b857285fc90cefab8bc7eb8fd50b3
ShravanCool <shravan.akku@gmail.com>
2025-05-25 19:35:14 +0530

    Added Zsh config

--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ ln -s "$HOME/dotfiles-arch/X11" "$HOME/.config"
 
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles-arch/i3" "$HOME/.config"
+
+#######
+# Zsh #
+#######
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles-arch/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles-arch/zsh/.zshrc" "$HOME/.config/zsh"
