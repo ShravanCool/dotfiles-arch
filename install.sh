c2e574cf66d4ddd86c76eacf359515c70fd721d5
ShravanCool <shravan.akku@gmail.com>
2025-05-26 10:46:46 +0530

    Added improvements to system

--- install.sh
+++ install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles-arch/X11" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 ######
 # i3 #
 ######
 
-rm -rf "$HOME/.config/i3"
-ln -s "$HOME/dotfiles-arch/i3" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
 #######
 # Zsh #
 #######
 
-mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles-arch/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/dotfiles-arch/zsh/.zshrc" "$HOME/.config/zsh"
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+
+#########
+# Fonts #
+#########
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#########
+# dunst #
+#########
+
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
